e2ea337cf3935dbfc1b82f07ceef4cfe4bc55164
Odedi <saoded@gmail.com>
2020-07-16 13:03:29 +0300

    arilou - multithread implementation (WIP)

--- unfinished/python/arilou/testing.py
+++ unfinished/python/arilou/testing.py
@@ -1,11 +1,11 @@
-import interview_task
+import can_module
 from datetime import datetime, timedelta
 from pause import until
 
 
 def selectors_unit_test():
     for i in range(0, 10):
-        print(interview_task.selectors(i))
+        print(can_module.selectors(i))
 
 
 def detector_unit_test():
@@ -17,45 +17,42 @@ def detector_unit_test():
     detector_length_unit_test()
 
 def detector_passing_unit_test():
-    interview_task.sys_reset()
+    can_module.sys_reset()
     last_gen_time = datetime.now()
 
     for i in range(1,4):
-        # print(interview_task.FRAME_META[0x100].timestamp)
-        frame = interview_task.frame_constructor(0x100*i, 0, 0)
+        frame = can_module.frame_constructor(0x100*i, 0, 0)
         # print(bin(frame))
         last_gen_time += timedelta(milliseconds=85)
         until(last_gen_time)  # REQUIRES PYTHON 3
         # SEND FRAME
-        interview_task.report(interview_task.detect_can_frame(frame))
+        can_module.report(can_module.detect_can_frame(frame))
 
 
 def detector_rate_unit_test():
-    interview_task.sys_reset()
+    can_module.sys_reset()
     last_gen_time = datetime.now()
 
     for i in range(3):
-        # print(interview_task.FRAME_META[0x100].timestamp)
-        frame = interview_task.frame_constructor(0x100, i, 0)
+        frame = can_module.frame_constructor(0x100, i, 0)
         # print(bin(frame))
         last_gen_time += timedelta(milliseconds=85)
         until(last_gen_time)  # REQUIRES PYTHON 3
         # SEND FRAME
-        interview_task.report(interview_task.detect_can_frame(frame))
+        can_module.report(can_module.detect_can_frame(frame))
 
 
 def detector_length_unit_test():
-    interview_task.sys_reset()
+    can_module.sys_reset()
     last_gen_time = datetime.now()
 
     for i in range(1,4):
-        # print(interview_task.FRAME_META[0x100].timestamp)
-        frame = interview_task.frame_constructor(0x200, 3, 0x010205 * i)
+        frame = can_module.frame_constructor(0x200, 3, 0x010205 * i)
         # print(bin(frame))
         last_gen_time += timedelta(milliseconds=101)
         until(last_gen_time)  # REQUIRES PYTHON 3
         # SEND FRAME
-        interview_task.report(interview_task.detect_can_frame(frame))
+        can_module.report(can_module.detect_can_frame(frame))
 
 # selectors_unit_test()
 detector_unit_test()
